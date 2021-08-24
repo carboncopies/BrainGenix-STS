3d78c8ca953652139e3b2afc77a1c3131f20fb57
tliao <tliao@carboncopies.org>
2021-08-24 15:34:44 -0700

    Add Logger Instantiation And System Config

--- Main.py
+++ Main.py
@@ -8,10 +8,18 @@ Description: This file is the main system file for STS.
 Date-Created: 2021-08-24
 '''
 
-from Core.Management.Logger.Logger import SysLog
+from Core.Initialization.LoadConfig import LoadLocalConfig
 
 
 ##############################################################################
 ## NOTE: A Lowercase "m" Preceeding A Class Means It's a Main System        ##
 ## NOTE: A Lowercase "s" Preceeding A Class Means It's a Subsystem          ##
 ##############################################################################
+
+
+# Load Config #
+SystemConfiguration = LoadLocalConfig(ConfigFilePath = 'Config.yaml')
+
+
+# Initialize Logger #
+mLogger = InstantiateLogger(SystemConfiguration)
