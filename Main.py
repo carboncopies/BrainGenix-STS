c22050ed0c06f58f2ecbb666a13a9eee5458234c
tliao <tliao@carboncopies.org>
2021-08-24 15:29:42 -0700

    Add Header To Main.py

--- Main.py
+++ Main.py
@@ -2,3 +2,9 @@
 ## This file is part of BrainGenix-STS ##
 #########################################
 
+'''
+Name: Main
+Description: This file is the main system file for STS.
+Date-Created: 2021-08-24
+'''
+
