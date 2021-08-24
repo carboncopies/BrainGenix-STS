c6972ee788f2ea379aa5b46bf1f5a182f7995b14
tliao <tliao@carboncopies.org>
2021-08-24 15:31:15 -0700

    Add Subsystem Naming Convention Notice

--- Main.py
+++ Main.py
@@ -11,3 +11,7 @@ Date-Created: 2021-08-24
 from Core.Management.Logger.Logger import SysLog
 
 
+##############################################################################
+## NOTE: A Lowercase "m" Preceeding A Class Means It's a Main System        ##
+## NOTE: A Lowercase "s" Preceeding A Class Means It's a Subsystem          ##
+##############################################################################
