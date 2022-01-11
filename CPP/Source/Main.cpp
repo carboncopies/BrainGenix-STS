88697321e71b420f59ae0036e78ab09dd54e92e4
datacrystals <tliao@carboncopies.org>
2022-01-11 23:50:09 +0000

    Implement CPP Build System

---
+++ CPP/Source/Main.cpp
@@ -0,0 +1,15 @@
+//=================================================================//
+// This file is part of the BrainGenix-STS Scan Translation System //
+//=================================================================//
+
+/*
+    Description: This is the main file of the system.
+    Additional Notes: None
+    Date Created: 2022-01-11
+*/
+
+
+int main() {
+
+    
+}
