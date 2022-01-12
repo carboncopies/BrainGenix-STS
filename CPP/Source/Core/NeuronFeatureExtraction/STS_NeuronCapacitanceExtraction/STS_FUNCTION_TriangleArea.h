0eda1d1f0f5d6012e76d10c7cc08e65a59fe97b8
datacrystals <tliao@carboncopies.org>
2022-01-12 00:47:41 +0000

    Create Triangle Struct

--- CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_FUNCTION_TriangleArea.h
+++ CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_FUNCTION_TriangleArea.h
@@ -2,8 +2,18 @@
 // This file is part of the BrainGenix-STS Scan Translation System //
 //=================================================================//
 
+// Standard Libraries
+#include <iostream>
+#include <memory>
+
+// Third-Party Libraries
+
+// Internal Libraries
+#include <STS_STRUCT_Triangle.h>
+
+
 /**
  * @brief This function is used to calculate the area of a triangle in xyz space.
  * 
  */
-double STS_FUNCTION_CalculatTriangleArea()
+double STS_FUNCTION_CalculateTriangleArea();
