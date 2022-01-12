19588490985c433e6f259ba3644d14032e093346
datacrystals <tliao@carboncopies.org>
2022-01-12 00:37:03 +0000

    Implement Triangle Area Extraction

--- CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_TriangleArea.h
+++ CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_TriangleArea.h
@@ -2,3 +2,8 @@
 // This file is part of the BrainGenix-STS Scan Translation System //
 //=================================================================//
 
+/**
+ * @brief This function is used to calculate the area of a triangle in xyz space.
+ * 
+ */
+double STS_FUNCTION_CalculatTriangleArea()
