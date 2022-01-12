27fcaa5c3d9f25c272dfdf2ebfbb59a906a4001f
datacrystals <tliao@carboncopies.org>
2022-01-12 23:56:36 +0000

    Add Boost Library

---
+++ test.py
@@ -0,0 +1,25 @@
+import math
+
+def find_traingle_area(Point_A, Point_B, Point_C):
+
+    # Extract Points
+    PointAX = Point_A[0]
+    PointAY = Point_A[1]
+    PointAZ = Point_A[2]
+
+    PointBX = Point_B[0]
+    PointBY = Point_B[1]
+    PointBZ = Point_B[2]
+
+    PointCX = Point_C[0]
+    PointCY = Point_C[1]
+    PointCZ = Point_C[2]
+    
+
+    # find line length
+
+    LineAB_Length = (math.abs(PointAX - PointBX) * )
+
+
+
+
