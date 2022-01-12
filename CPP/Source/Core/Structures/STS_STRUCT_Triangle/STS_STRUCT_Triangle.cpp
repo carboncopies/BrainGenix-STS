3c50466e69956044f5180fd0da6ce06e55cbd65e
datacrystals <tliao@carboncopies.org>
2022-01-12 00:42:22 +0000

    Create Triangle Struct

--- CPP/Source/Core/Structures/STS_STRUCT_Triangle/STS_STRUCT_Triangle.cpp
+++ CPP/Source/Core/Structures/STS_STRUCT_Triangle/STS_STRUCT_Triangle.cpp
@@ -1,3 +1,25 @@
 //=================================================================//
 // This file is part of the BrainGenix-STS Scan Translation System //
 //=================================================================//
+
+#include <STS_STRUCT_Triangle.h>
+
+
+struct STS_STRUCT_Triangle {
+
+    // First Vert
+    double X1;
+    double Y1;
+    double Z1;
+
+    // Second Vert
+    double X2;
+    double Y2;
+    double Z2;
+
+    // Third Vert
+    double X3;
+    double Y3;
+    double Z3;
+
+};
--- CPP/Source/Core/Structures/STS_STRUCT_Triangle/STS_STRUCT_Triangle.h
+++ CPP/Source/Core/Structures/STS_STRUCT_Triangle/STS_STRUCT_Triangle.h
@@ -1,3 +1,12 @@
 //=================================================================//
 // This file is part of the BrainGenix-STS Scan Translation System //
 //=================================================================//
+
+
+
+
+/**
+ * @brief This structure stores a triangle with x,y,z for all three vertices.
+ * 
+ */
+struct STS_STRUCT_Triangle;
