a1e4b52fef1f3e94deb6ab01fdb37765e98bb834
datacrystals <tliao@carboncopies.org>
2022-01-13 07:25:36 +0000

    Create Triangle Area Multithreading Engine

---
+++ CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_CLASS_TriangleAreaThreadingEngine.h
@@ -0,0 +1,31 @@
+//=================================================================//
+// This file is part of the BrainGenix-STS Scan Translation System //
+//=================================================================//
+
+#pragma once
+
+// Standard Libraries
+#include <iostream>
+#include <memory>
+#include <thread>
+
+// Third-Party Libraries
+
+// Internal Libraries
+
+
+/**
+ * @brief Multithreaded System For Finding Triangle Areas
+ * 
+ */
+class STS_CLASS_TriangleAreaThreadingEngine {
+
+private:
+
+
+public:
+
+
+
+
+};
