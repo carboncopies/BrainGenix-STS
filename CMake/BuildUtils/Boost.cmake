96b7c3b36915175960ee86c55840c7a42e17d59c
datacrystals <tliao@carboncopies.org>
2022-01-13 23:51:30 +0000

    Change Boost Version

--- CMake/BuildUtils/Boost.cmake
+++ CMake/BuildUtils/Boost.cmake
@@ -3,6 +3,6 @@
 ##########################
 
 STSBuildLogger(${Green} "Configuring Boost Library")
-set(FETCHCONTENT_SOURCE_DIR_BOOST "${LIB_DIR}/boost")
+#set(FETCHCONTENT_SOURCE_DIR_BOOST "${LIB_DIR}/boost")
 add_subdirectory("${LIB_DIR}/boost-cmake")
 STSBuildLogger(${BoldGreen} "Finished Configuring Boost Library")
