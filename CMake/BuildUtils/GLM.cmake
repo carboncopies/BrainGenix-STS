38af960bc91b58c64ae2e6b1b56f461c144c5a19
datacrystals <tliao@carboncopies.org>
2022-01-13 21:44:46 +0000

    Add Model Loader Libraries

--- CMake/BuildUtils/GLM.cmake
+++ CMake/BuildUtils/GLM.cmake
@@ -3,7 +3,6 @@
 ##########################
 
 STSBuildLogger(${Green} "Configuring GLM Library")
-set(GLM_STATIC_LIBRARY_ENABLE ON)
 add_subdirectory(${LIB_DIR}/glm)
 include_directories(${LIB_DIR}/glm)
 STSBuildLogger(${BoldGreen} "Finished Configuring GLM Library")
