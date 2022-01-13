f48f9b50c2002db29a80fd36d5f7e259873abd59
datacrystals <tliao@carboncopies.org>
2022-01-13 07:31:37 +0000

    Create Triangle Area Multithreading Engine

--- CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_CLASS_TriangleAreaThreadingEngine.cpp
+++ CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_CLASS_TriangleAreaThreadingEngine.cpp
@@ -2,3 +2,24 @@
 // This file is part of the BrainGenix-STS Scan Translation System //
 //=================================================================//
 
+#include <STS_CLASS_TriangleAreaThreadingEngine.h>
+
+
+// Constructor
+STS_CLASS_TriangleAreaThreadingEngine::STS_CLASS_TriangleAreaThreadingEngine(std::shared_ptr<STS_STRUCT_SystemUtils> SystemUtils) {
+
+    // Copy Pointers
+    SystemUtils_ = SystemUtils;
+
+    // Log Initialization
+    SystemUtils_->Logger_->Log("Initializing Triangle Area Threading Engine", 5);
+
+}
+
+// Destructor
+STS_CLASS_TriangleAreaThreadingEngine::~STS_CLASS_TriangleAreaThreadingEngine() {
+
+    // Log Destructor Call
+    SystemUtils_->Logger_->Log("Triangle Area Threading Engine Destructor Called", 6);
+
+}
