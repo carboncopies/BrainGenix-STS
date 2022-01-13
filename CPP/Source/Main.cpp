ade412ee5a21a23e9cc475833a3fdc153c2bfe21
datacrystals <tliao@carboncopies.org>
2022-01-13 00:50:24 +0000

    Test Distance Function

--- CPP/Source/Main.cpp
+++ CPP/Source/Main.cpp
@@ -8,8 +8,21 @@
     Date Created: 2022-01-11
 */
 
+#include <STS_FUNCTION_CalculateLineDistance.h>
 
 int main() {
 
+    STS_STRUCT_Vertex V1;
+    STS_STRUCT_Vertex V2;
+    
+    V1.X = 3;
+    V1.Y = 3;
+    V1.Z = 3;
+
+    V2.X = 0;
+    V2.Y = 0;
+    V2.Z = 0;
+
+    std::cout<<STS_FUNCTION_CalculateLineDistance(V1, V2)<<std::endl;
     
 }
