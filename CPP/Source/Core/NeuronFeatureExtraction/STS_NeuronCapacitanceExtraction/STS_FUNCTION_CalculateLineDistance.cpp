d6de33f6e7be75ebc279de70498c37980602a48a
datacrystals <tliao@carboncopies.org>
2022-01-13 01:45:12 +0000

    Optimize Distance Calculation Speed

--- CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_FUNCTION_CalculateLineDistance.cpp
+++ CPP/Source/Core/NeuronFeatureExtraction/STS_NeuronCapacitanceExtraction/STS_FUNCTION_CalculateLineDistance.cpp
@@ -7,31 +7,19 @@
 // Get Line Distance
 double STS_FUNCTION_CalculateLineDistance(STS_STRUCT_Vertex V1, STS_STRUCT_Vertex V2) {
 
-    // Calculate Components
-    double XComp = (V1.X - V2.X) * (V1.X - V2.X);
-    double YComp = (V1.X - V2.X) * (V1.X - V2.X);
-    double ZComp = (V1.Z - V2.Z) * (V1.Z - V2.Z);
 
-    // Sum Components, Take Sqrt
-    return sqrt((XComp + YComp + ZComp));
+    // Calculate Distance, Return. Is The Same As More Readable Version Below
+    return sqrt(((V1.X - V2.X) * (V1.X - V2.X) + (V1.X - V2.X) * (V1.X - V2.X) + (V1.Z - V2.Z) * (V1.Z - V2.Z)));
 
 
+    // // Calculate Components
+    // double XComp = (V1.X - V2.X) * (V1.X - V2.X);
+    // double YComp = (V1.X - V2.X) * (V1.X - V2.X);
+    // double ZComp = (V1.Z - V2.Z) * (V1.Z - V2.Z);
 
-    // double XY =  ((V1.X, V2.X) * (V1.X, V2.X) + (V1.Y - V2.Y) * (V1.Y - V2.Y));
-    // double Length = sqrt( XY + ((V1.X - V2.Z) * V1.Z - V2.Z));
+    // // Sum Components, Take Sqrt
+    // return sqrt((XComp + YComp + ZComp));
 
 
-    // return Length;
+
 }
-
-
-/*
-    def find_line_length(X1, Y1, Z1, X2, Y2, Z2):
-
-        XY = (math.sqrt((abs(X1 - X2) * abs(X1 - X2)) + (abs(Y1 - Y2) * abs(Y1 - Y2))))
-    
-        Length = (math.sqrt((XY * XY) + (abs(Z1 - Z2) * abs(Z1 - Z2))))
-
-        return Length 
- */
-
