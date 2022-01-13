7dac6a18e8447d17b33ef41a73bb4fb9756648c2
datacrystals <tliao@carboncopies.org>
2022-01-13 00:28:44 +0000

    Implement TriangleArea Function

--- CPP/Source/Core/Structures/STS_STRUCT_Triangle/STS_STRUCT_Triangle.cpp
+++ CPP/Source/Core/Structures/STS_STRUCT_Triangle/STS_STRUCT_Triangle.cpp
@@ -3,13 +3,3 @@
 //=================================================================//
 
 #include <STS_STRUCT_Triangle.h>
-
-
-struct STS_STRUCT_Triangle {
-
-    // Vertexes
-    STS_STRUCT_Vertex V1;
-    STS_STRUCT_Vertex V2;
-    STS_STRUCT_Vertex V3;
-
-};
