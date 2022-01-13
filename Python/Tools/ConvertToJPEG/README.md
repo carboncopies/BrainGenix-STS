662e80661bc93d551ada54c1c07f408845181b97
datacrystals <tliao@carboncopies.org>
2022-01-13 21:03:21 +0000

    Move CPP To Root Repo Dir

--- Python/Tools/ConvertToJPEG/README.md
+++
@@ -1,1 +0,0 @@
-This tool converts from the neuroglancer .shard format to raw jpeg images.
--- Python/Tools/ConvertToJPEG/Scripts/Install.sh
+++
@@ -1,7 +0,0 @@
-# Get Python
-sudo apt install python3 -y
-sudo apt install python3-pip -y
-
-
-# Get packages
-pip3 install cloud-volume
