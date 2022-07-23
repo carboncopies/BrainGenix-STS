214563ba0265438b1f6cd1b45a0ac7f34a07f4eb
madmax3000 <johnjose223@gmail.com>
2022-07-23 07:49:16 -0700

    commit 07/23/22
    1. made the functions into classes
    2. commented in all the basic required info needed
    3. file handling has some issues
    4. committing and pushing to stitch branch
    5. still in development
    6. pushing since my vm is crashing and need to have the data stored reliable location

---
+++ Sandbox/Stitch/layermerge.py
@@ -0,0 +1,63 @@
+#############################################################################
+# this code merges multiple slices into the rgb or any composite form we need.
+#############################################################################
+
+
+import imagej
+import scyjava as sj
+import os.path
+
+# initialize ImageJ2
+ij = imagej.init(mode='interactive')
+# import ImageJ classes
+IJ = sj.jimport('ij.IJ')
+
+'''
+slice merge class 
+slice merge class has functions which takes image slices and convert into compresses rgb images or 
+any such composite images
+'''
+
+
+class SliceMerge:
+    def __init__(self):
+
+        # by default DataLocation and OutputDirectory variables point to test data provided with the module.
+        self.DataLocation = '/home/jj/PycharmProjects/BrainGenix-STS/Sandbox/Stitch/output'
+
+        '''
+        stack to rgb function
+        
+        This function takes slices of images and convert it into image stacks . 
+        These image stacks are then converted into 24bit rgb images from the 3 images in the stack
+        '''
+
+    def StackToRGB(self, DataLocation):
+        self.DataLocation = DataLocation
+        macrotest = """
+        run("Image Sequence...", "dir=""" + self.DataLocation + """/ sort");
+        run("Stack to RGB");
+        saveAs("Jpeg", \"""" + self.DataLocation + """/result.jpg");
+
+        """
+        print(macrotest)
+        IJ.runMacro(macrotest)
+
+        print(" rgb image generated")
+
+        '''
+        test function
+        
+        test function testes all the methods in the given class Slice_merge
+        '''
+
+    def Test(self):
+        #os.remove("output/result.jpg")
+        self.StackToRGB(self.DataLocation)
+        if os.path.exists("output/result.jpg"):
+            print("stack to rgb functionality verified")
+        else:
+            print(" stack to rgb functionality failed")
+
+
+
